a4b00dd182ae115851c064df64375c9434affabb
Aron Elias HERRERA PONTE <aron.herrera@skaleet.com>
2024-07-31 23:49:07 +0200

    Add Skaleet Frontend Technical Test project

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['@react-native-community', 'plugin:@typescript-eslint/recommended'],
+};
+ 
---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  printWidth: 80,
+  useTabs: false,
+};
